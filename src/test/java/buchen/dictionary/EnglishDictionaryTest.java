cfab832c6afbd369267a489c150b70eecd9327c8
Aliza Buchen <alizabuchen@Alizas-MacBook-Air-2.local>
2024-04-10 14:22:03 -0400

    checkstyle fixes

--- src/test/java/buchen/dictionary/EnglishDictionaryTest.java
+++ src/test/java/buchen/dictionary/EnglishDictionaryTest.java
@@ -1,28 +1,4 @@
 package buchen.dictionary;
-//
-//import com.opencsv.exceptions.CsvValidationException;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.IOException;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class EnglishDictionaryTest {
-//
-//    @Test
-//    public void getDefinition() throws CsvValidationException, IOException {
-//        // given
-//        EnglishDictionary dictionary = new EnglishDictionary();
-//
-//        // when
-//        List<String> definitions = dictionary.getDefinition("slee");
-//
-//        // then
-//        assertEquals("[[To slay.]]", definitions);
-//    }
-//
-//}
 
 import com.opencsv.exceptions.CsvValidationException;
 import org.junit.jupiter.api.Test;
@@ -44,7 +20,8 @@ public class EnglishDictionaryTest {
 
         //then
         List<String> expected = Arrays.asList("[One and one,  twice one.]",
-                "[The sum of one and one,  the number next greater than one, and next less than three,  two units or objects.]",
+                "[The sum of one and one,  the number next greater than one, and next less than three,  " +
+                        "two units or objects.]",
                 "[A symbol representing two units, as 2, II., or ii.]");
 
         assertEquals(expected, definitions);
